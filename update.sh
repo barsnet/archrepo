d15e4307cd7900e962bc1f731459b8d69dc09966
Vova Kolinko <vovakolinko@gmail.com>
2022-05-16 16:29:06 +0300

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 rm -rfv x86_64/archrepo.db x86_64/archrepo.db.tar.gz x86_64/archrepo.files.tar.gz x86_64/archrepo.files
-repo-add x86_64/archrepo.db.tar.gz x86_64/*/*.pkg.tar.zst
+repo-add x86_64/archrepo.db.tar.gz x86_64/*.pkg.tar.zst
 git add .
 git commit -m 'update'
 git push
